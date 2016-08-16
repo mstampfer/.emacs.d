52207c4d150fe63dc4d620dc1d70dded27da554f
Marcel Stampfer <git@axonconsulting.com>
2016-08-16 20:08:51 +0100

    updated

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 
 
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://http://melpa.milkbox.net/packages/"))
 
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
