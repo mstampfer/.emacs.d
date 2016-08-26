b37686c3a774107a868f062ab9ed63e36ca05bdd
Marcel Stampfer <git@axonconsulting.com>
2016-08-26 22:19:08 +0100

    added ein, realgud keymaps

--- init.el
+++ init.el
@@ -30,6 +30,8 @@
 		   auto-complete
 		   autopair
 		   bind-key
+		   color-theme
+		   load-theme-buffer-local
 		   find-file-in-repository
 		   flx-ido
 		   flycheck
@@ -101,6 +103,10 @@
 ;; the background, type C-u F.
 (require 'dired-x)
 
+;;Emacs IPython Notebook (EIN) provides a IPython Notebook client and
+;;integrated REPL (like SLIME) in Emacs.
+(require 'ein)
+
 (global-flycheck-mode t)
 
 ;; open browser with search pattern in Google01
@@ -109,13 +115,16 @@
 ;; Use Helm to match M-x occur patterns
 (global-set-key [f6] 'helm-occur)
 
-;;(global-set-key [f7] 'find-file-in-repository)
+(global-set-key [f7] 'realgud:trepan2)
 
 ;;Recursively grep for REGEXP in FILES in directory tree rooted at DIR
 (global-set-key [f8] 'rgrep)
 
 ;; Run projectile or helm-for-files depending if cwd is a project 
 (global-set-key [f9] 'cwd-open-file)
+
+;; Run Ein notebooklist-open 
+(global-set-key [f10] 'ein:notebooklist-open)
 
 ;; deleting duplicate lines is a common operation when working with
 ;; data. This function doesn't require the region to be sorted. Bind
@@ -138,7 +147,12 @@
 ;; tell emacs how to parse Tex
 (add-hook 'tex-mode-hook #'(lambda () (setq ispell-parser 'tex)))
 
- 
+;;(require 'color-theme-buffer-local)
+;;   (add-hook 'ein-mode 
+;;     (lambda nil (color-theme-buffer-local 'color-theme-light-blue (current-buffer))))
+
+;; Ein settings
+
 ;; Jedi settings
 (require 'jedi)
 (jedi:install-server)			
@@ -209,16 +223,85 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(compilation-message-face (quote default))
  '(completion-styles (quote (substring)))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
    (quote
-    ("cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("82b67c7e21c3b12be7b569af7c84ec0fb2d62105629a173e2479e1053cff94bd" "cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(delete-selection-mode t)
  '(dired-listing-switches "-alrt")
+ '(fci-rule-color "#073642")
  '(global-subword-mode t)
  '(global-superword-mode t)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(icicle-mode t)
- '(kill-whole-line t))
+ '(kill-whole-line t)
+ '(magit-diff-use-overlays nil)
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#c85d17")
+     (60 . "#be730b")
+     (80 . "#b58900")
+     (100 . "#a58e00")
+     (120 . "#9d9100")
+     (140 . "#959300")
+     (160 . "#8d9600")
+     (180 . "#859900")
+     (200 . "#669b32")
+     (220 . "#579d4c")
+     (240 . "#489e65")
+     (260 . "#399f7e")
+     (280 . "#2aa198")
+     (300 . "#2898af")
+     (320 . "#2793ba")
+     (340 . "#268fc6")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(xterm-color-names
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+   ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 
 ;; Rebind "M-o" to move between windows duplicating "C-o" since its
 ;; a very common thing to do
