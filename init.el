2575702609492f7d36ecd79368ee418a4ee59577
Marcel Stampfer <git@axonconsulting.com>
2016-08-27 22:51:47 +0100

    customization values

--- init.el
+++ init.el
@@ -270,6 +270,7 @@
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
+ '(request-backend (quote url-retrieve))
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
  '(term-default-bg-color "#002b36")
  '(term-default-fg-color "#839496")
